c85558d284391875a645374fe0eaaf4988ad51fa
Viesturs Teivāns <vteivans@gmail.com>
2021-03-01 10:08:21 +0200

    pr. 1. mar. 2021
    - click event listener

--- script.js
+++ script.js
@@ -4,4 +4,13 @@ let sveikiPoga = document.querySelector('#sveiki-poga');
 console.log('input value:', sveikiInput.value);
 console.log('poga', sveikiPoga);
 
+function sveicinat(vards) {
+    return 'Sveicināts, ' + vards + '!'; // Sveiki, Reinis!
+}
+
+sveikiPoga.addEventListener('click', function() {
+    let sveiciens = sveicinat(sveikiInput.value);
+    alert(sveiciens);
+});
+
 
