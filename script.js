cc24ee15f2a845320ac6f46946f724162511d8a3
Viesturs Teivāns <vteivans@gmail.com>
2021-03-22 07:50:50 +0200

    ct. 9. mar. 2021
    - Paragrāfa teksta aizstāšana ar citu tekstu
    - Lietotāja datu ievades pārbaude

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 let sveikiInput = document.querySelector('#sveiki-input');
 let sveikiPoga = document.querySelector('#sveiki-poga');
+let sveikiIzvade = document.querySelector('#sveiki-izvade');
 
 console.log('input value:', sveikiInput.value);
 console.log('poga', sveikiPoga);
+console.log('izvade', sveikiIzvade);
 
 function sveicinat(vards) {
-    return 'Sveicināts, ' + vards + '!'; // Sveiki, Reinis!
+    return 'Čau, ' + vards + '!'; // Čau, Reinis!
 }
 
+// Reaģējam uz klikšķi
 sveikiPoga.addEventListener('click', function() {
-    let sveiciens = sveicinat(sveikiInput.value);
-    alert(sveiciens);
+    if (sveikiInput.value) {
+        let sveiciens = sveicinat(sveikiInput.value);
+        sveikiIzvade.innerText = sveiciens;
+    } else {
+        alert('Lūdzu ievadi vērtību!');
+    }
 });
-
-
