eb9b147066dd7768b1c4f2f94dd1ddb5d809e61f
Viesturs Teivāns <vteivans@gmail.com>
2021-03-22 07:50:03 +0200

    ct. 12. mar. 2021
    - Paragrāfa teksta aizstāšana ar citu tekstu
    - Lietotāja datu ievades pārbaude

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ console.log('sveiki input', sveikiInput);
 let sveikiPoga = document.querySelector('#sveiki-poga');
 console.log('poga', sveikiPoga);
 
+let sveikiIzvade = document.querySelector('#sveiki-izvade');
+console.log(sveikiIzvade);
+
 // Funkcijas definīcija
 function sveicinat(vards) {
     return 'Čau, ' + vards + '!';
@@ -12,6 +15,11 @@ function sveicinat(vards) {
 // Ierindas funkcija - inline function
 sveikiPoga.addEventListener('click', function() {
     let vards = sveikiInput.value;
-    let sveiciens = sveicinat(vards);
-    console.log(sveiciens);
+
+    if (vards) {
+        let sveiciens = sveicinat(vards);
+        sveikiIzvade.innerText = sveiciens;
+    } else {
+        alert('Lūdzu ievadi vērtību!');
+    }
 });
